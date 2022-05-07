643bc16c31c54f84992ae19c8067981540561397
arjunTheCoder <82092827+arjun380@users.noreply.github.com>
2022-05-07 08:07:26 +0530

    Add files via upload

---
+++ C-170.py
@@ -0,0 +1,33 @@
+class history_space:
+    def t2008(self):
+        print("\n The year 2008 contained several significant events in spaceflight, including the first flyby of Mercury by a spacecraft since 1975, the discovery of water ice on Mars by the Phoenix spacecraft, which landed in May, the first Chinese spacewalk in September, the launch of the first Indian Lunar probe in October,")
+        
+    def t2011(self):
+        print("\n The first crewed flight of 2011 was STS-133, the final flight of the Space Shuttle Discovery, which launched from the Kennedy Space Center on 24 February. STS-133 carried Leonardo, the final American pressurised module of the ISS, for installation. Discovery returned to Earth on 9 March.")
+
+class history_old:
+    def t2008(self):
+        print("\n 26–29 November – 2008 Mumbai attacks: 175 people were killed and over 308 wounded in more than ten coordinated shooting and bombing attacks across Mumbai, India's largest city, carried out by Islamic terrorists from Pakistan. Mumbai Urban Transport Project is completed. Bangalore International Airport is completed")
+    
+    def t2011(self):
+        print("\n Japan earthquake and tsunami of 2011, also called Great Sendai Earthquake or Great Tōhoku Earthquake, severe natural disaster that occurred in northeastern Japan on March 11, 2011. The event began with a powerful earthquake off the northeastern coast of Honshu, Japan’s main island, which caused widespread damage on land and initiated a series of large tsunami waves that devastated many coastal areas of the country, most notably in the Tōhoku region (northeastern Honshu). The tsunami also instigated a major nuclear accident at a power station along the coast.")
+
+class history_sports:
+    def t2008(self):
+        print("\n Boston Celtics win their seventeenth NBA Championship by defeating the Los Angeles Lakers in six games in the 2008 NBA Finals. Paul Pierce was named Finals MVP")
+        
+    def t2011(self):
+        print("\n The year 2011 was another great one on the sporting world. In major sporting events, Japan won the Women's Football World Cup in a tight game against the US.")
+        
+
+sports = history_sports()
+sports.t2008()
+sports.t2011()
+
+space = history_space()
+space.t2008()
+space.t2011()
+
+old = history_old()
+old.t2008()
+old.t2011()
